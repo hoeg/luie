775973fa16703585766754bb9ee27ce4f0b8bbbe
hoeg <peter.steffensen@gmail.com>
2017-09-30 20:37:33 +0000

    added structs and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # luie
+
+## TODO
+- Load all script contained in a folder into list of runnable scripts
+- inf loop wating for an identifier and the arguments to the script
+- handle arguments
+- think about security measures wrt. loading scripts
+- decouple main loop from execution of scripts (network execution)
--- main.c
+++ main.c
@@ -5,6 +5,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct Code {
+  uint32_t size;
+  uint8_t *data;
+} Code;
+
+typedef struct Argument {
+  union {
+    uint8_t
+    uint32_t
+  } value;
+} Argument;
+
+typedef struct Arguments {
+  uint32_t size;
+  Argument *args;
+} Arguments;
+
+typedef struct RunnableScript {
+  uint32_t identifier;
+  Code code;
+  Arguments argSpec;
+} RunnableScript;
+
+typedef struct RunnableScripts {
+  RunnableScript *scripts;
+  uint32_t size;
+} RunnableScripts;
+
 void bail(lua_State *L, char *msg){
   fprintf(stderr, "\nFATAL ERROR:\n  %s: %s\n\n",
     msg, lua_tostring(L, -1));
